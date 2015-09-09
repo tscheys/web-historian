2c82e31c8c8f52156ea221e0569d50a6557cacef
tscheys <tim.scheys@gmail.com>
2015-09-09 11:31:15 -0700

    Add router and begin basic url routing

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,14 +1,22 @@
 var http = require("http");
 var handler = require("./request-handler");
 var initialize = require("./initialize.js");
-
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
 // Why do you think we have this here?
 // HINT: It has to do with what's in .gitignore
 initialize("./archives");
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+var router = {
+  '/': handler.handleRequest
+};
+var server = http.createServer(function (req, res) {
+  var route = url.parse(req.url).pathname;
+
+});
 
 if (module.parent) {
   module.exports = server;
@@ -16,4 +24,6 @@ if (module.parent) {
   server.listen(port, ip);
   console.log("Listening on http://" + ip + ":" + port);
 }
+
+
 
